41a6b1b8ff14d50873b9d328e7eed90ca78679d2
G.M <47867339+GMLJ@users.noreply.github.com>
2021-09-25 19:49:06 +0200

    categories routes/database config

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
 
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -15,9 +17,26 @@ mongoose
   .then(console.log("connected to MongoDB"))
   .catch((err) => console.log(err));
 
+// STORE USER IMAGE
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+// UPLOAD USER IMAGE
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File uploaded");
+});
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Back end is running");
