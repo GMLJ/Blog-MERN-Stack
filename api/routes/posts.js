d9bc42cde445be5ee3b5da7507026ab6cc1d4f8b
G.M <47867339+GMLJ@users.noreply.github.com>
2021-09-25 16:35:02 +0200

    delete / get / get all posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -16,32 +16,76 @@ router.post("/", async (req, res) => {
 
 //UPDATE POST
 router.put("/:id", async (req, res) => {
-  if (req.body.postId === req.params.id) {
-    try {
-      const updatedPost = await Post.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedPost);
-    } catch (err) {
-      res.status(404).json("Post not found");
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can update your posts only");
     }
-  } else {
-    res.status(401).json("You can update your posts only");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 //DELETE POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete your posts only");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //GET POST
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POST
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const categoryName = req.query.category;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username: username });
+    } else if (categoryName) {
+      posts = await Post.find({
+        categories: {
+          $in: [categoryName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
