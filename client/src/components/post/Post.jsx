c9ca580f3e2213a47f85c40333d4b07071bcf5c6
G.M <47867339+GMLJ@users.noreply.github.com>
2021-09-26 19:20:12 +0200

    hooks implementation react

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,18 +1,24 @@
-import "./post.css"
+import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
-      <img src="https://images.pexels.com/photos/6212576/pexels-photo-6212576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="travel on bike" className="postImg" />
+      {post.photo && (
+        <img src={post.photo} alt="" className="postImg" />
+      )}
       <div className="postInfo">
         <div className="postCategories">
-          <span className="postCategory">MUSIC</span>
-          <span className="postCategory">LIFE</span>
+          {post.categories.map((c) => (
+            <span className="postCategory">{c.name}</span>
+          ))}   
         </div>
-        <div className="postTitle">Lorem ipsum dolor</div>
+        <Link to={`/post/${post._id}`} className="link">
+        <span className="postTitle">{post.title}</span>
+        </Link>
         <hr/>
-        <div className="postDate">1 hour ago</div>
-        <p className="postDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi obcaecati ipsam earum. Est itaque quis tempore, temporibus inventore totam enim sequi aut? Impedit labore accusantium a suscipit cupiditate, repellat molestias.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi obcaecati ipsam earum. Est itaque quis tempore, temporibus inventore totam enim sequi aut? Impedit labore accusantium a suscipit cupiditate, repellat molestias.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi obcaecati ipsam earum. Est itaque quis tempore, temporibus inventore totam enim sequi aut? Impedit labore accusantium a suscipit cupiditate, repellat molestias.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi obcaecati ipsam earum. Est itaque quis tempore, temporibus inventore totam enim sequi aut? Impedit labore accusantium a suscipit cupiditate, repellat molestias.</p>
+        <div className="postDate">{new Date(post.createdAt).toDateString()}</div>
+        <p className="postDescription">{post.description}</p>
       </div>
     </div>
   )
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,34 +1,45 @@
-import "./singlePost.css"
+import { useEffect, useState } from "react";
+import { useLocation} from "react-router-dom";
+import axios from "axios";
+import "./singlePost.css";
 
 export default function SinglePost() {
+
+  const location = useLocation()
+  const path = location.pathname.split("/")[2];
+
+  const [post, setPost] = useState({});
+
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    }
+    getPost();
+  },[path])
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
+        {post.photo && (
         <img 
-        src="https://images.pexels.com/photos/1416169/pexels-photo-1416169.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
-        alt="travel bike" 
+        src={post.photo} 
+        alt="" 
         className="singlePostImg" 
         />
+        )}
         <h1 className="singlePostTitle">
-          Lorem ipsum dolor.
+          {post.title}
           <div className="singlePostEdit">
           <i className="singlePostIcon far fa-edit"></i>
           <i className="singlePostIcon far fa-trash-alt"></i>
           </div>
         </h1>
         <div className="singlePostInfo">
-          <span className="singlePostAuthor">Author: <b>Matty</b></span>
-          <span className="singlePostDate">1 hour ago</span>
+          <span className="singlePostAuthor">Author: <b>{post.username}</b></span>
+          <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
         </div>
-        <p className="singlePostDescription">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus veritatis quos iste pariatur corrupti laboriosam alias unde, eius nulla ullam corporis deserunt, autem maiores! Non, quidem cumque! Perspiciatis, asperiores incidunt.
-        Consectetur laborum odit repellendus cumque maiores ducimus illum ea ratione incidunt excepturi necessitatibus perferendis earum a adipisci natus, explicabo sed fugiat neque nulla? Aperiam porro ea dolor, consequatur vel harum!
-        Quia, obcaecati! Rerum ipsa consequatur amet possimus dicta nisi error sequi? Nemo nisi eligendi consequuntur ea porro commodi tempora, placeat, architecto eveniet voluptate delectus, perspiciatis fugit odio dolores soluta doloribus.
-        Ex, doloribus! Magnam repellendus harum quidem est ab illum modi a expedita exercitationem veritatis dignissimos natus, blanditiis autem ducimus fugiat, adipisci necessitatibus iure voluptas nesciunt totam assumenda amet, repudiandae similique!
-        Eius, dicta? Quisquam consequuntur laborum earum, deserunt asperiores rem voluptatem? Architecto illo, accusamus aliquid dolor dolorem libero maxime voluptatum ab qui quam cupiditate impedit tempore similique eos officiis fuga vitae.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus veritatis quos iste pariatur corrupti laboriosam alias unde, eius nulla ullam corporis deserunt, autem maiores! Non, quidem cumque! Perspiciatis, asperiores incidunt.
-        Consectetur laborum odit repellendus cumque maiores ducimus illum ea ratione incidunt excepturi necessitatibus perferendis earum a adipisci natus, explicabo sed fugiat neque nulla? Aperiam porro ea dolor, consequatur vel harum!
-        Quia, obcaecati! Rerum ipsa consequatur amet possimus dicta nisi error sequi? Nemo nisi eligendi consequuntur ea porro commodi tempora, placeat, architecto eveniet voluptate delectus, perspiciatis fugit odio dolores soluta doloribus.
-        Ex, doloribus! Magnam repellendus harum quidem est ab illum modi a expedita exercitationem veritatis dignissimos natus, blanditiis autem ducimus fugiat, adipisci necessitatibus iure voluptas nesciunt totam assumenda amet, repudiandae similique!
-        Eius, dicta? Quisquam consequuntur laborum earum, deserunt asperiores rem voluptatem? Architecto illo, accusamus aliquid dolor dolorem libero maxime voluptatum ab qui quam cupiditate impedit tempore similique eos officiis fuga vitae.</p>
+        <p className="singlePostDescription">{post.description}</p>
       </div>
     </div>
   )
