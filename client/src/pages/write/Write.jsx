49e000edf808510af2b3bf0c91e66534021079ba
G.M <47867339+GMLJ@users.noreply.github.com>
2021-09-30 01:20:54 +0200

    minor update

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,26 +1,50 @@
-import "./write.css"
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context";
+import "./write.css";
 
 export default function Write() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      username: user.username,
+      title,
+      description,
+    };
+    if (file) {
+      const data = FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+    }
+
+    axios.post("/posts");
+  };
+
   return (
     <div className="write">
-      <img 
-      src="https://images.pexels.com/photos/1103995/pexels-photo-1103995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
-      alt="" 
-      className="writeImg" 
-      />
-      <form className="writeForm">
+      <img src="https://images.pexels.com/photos/1103995/pexels-photo-1103995.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="writeImg" />
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
-          <i className="writeIcon fas fa-plus"></i>
+            <i className="writeIcon fas fa-plus"></i>
           </label>
-          <input type="file" id="fileInput" style={{display:"none"}}/>
-          <input type="text" placeholder="Title" className="writeInput" autoFocus="true"/>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input type="text" placeholder="Title" className="writeInput" autoFocus="true" />
         </div>
         <div className="writeFormGroup">
           <textarea type="text" className="writeInput writeText" placeholder="Write article here..."></textarea>
         </div>
-        <button className="writeSubmit">Publish</button>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
       </form>
     </div>
-  )
+  );
 }
